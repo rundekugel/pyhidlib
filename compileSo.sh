07093e7d13ff370baa85dba1043611dbea512d08
rundekugel <rundekugel+gh@gmail.com>
2014-12-17 11:38:24 +0100

    more comment

--- compileSo.sh
+++ compileSo.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-#build the pyhidlib.so
+# Copyright (c) 2014 lifesim.de
+#
+# URL: https://github.com/rundekugel/pyhidlib
+#
+# Licensed under the MIT license:
+#    http://www.opensource.org/licenses/mit-license.php
+
+echo brought to you by lifesim.de
+echo building pyhidlib.so...
 
 if ! [ -d "build" ]; then
 mkdir build
 fi
 
 gcc  -c -fpic  src/pyhidlib.c -o build/pyhidlib.o
-
 gcc -shared -lc  -o build/pyhidlib.so  build/pyhidlib.o
 
-echo if everything went good, find the result in: build/pyhidlib.so
+echo done. if everything went good, find the result in: build/pyhidlib.so
 echo execute:
 echo sudo cp build/pyhidlib.so /opt/lib
 echo to use it comfortable
